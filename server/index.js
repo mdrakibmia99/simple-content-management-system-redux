d8317a783018904f54fd0e1ba9f709d4b5294847
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-12-13 16:38:58 +0600

    backend enternal imports added

---
+++ server/index.js
@@ -0,0 +1,5 @@
+/* external imports */
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
