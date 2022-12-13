78c1e2f22763ef599ee890df52a75ae2678c1237
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-12-13 16:07:36 +0600

    create a hook for blogs

---
+++ client/src/hooks/useBlogs.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useBlogs() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://simple-content-management-system-redux.vercel.app/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return blogs;
+}
+
+export default useBlogs;
