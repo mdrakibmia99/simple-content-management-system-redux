eb0e6248db816b068df4fe3a3d431f637c9dc6bb
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-12-13 16:40:30 +0600

    middlewares connection done and connect with mongodb

--- server/index.js
+++ server/index.js
@@ -7,3 +7,14 @@ require("dotenv").config();
 /* application level connection */
 const app = express();
 const port = process.env.PORT || 5000;
+
+/* middlewares connection */
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todoapp.mfoky.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
